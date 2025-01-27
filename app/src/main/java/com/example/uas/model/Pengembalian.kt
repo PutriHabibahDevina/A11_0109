900db322b4c78e4cae79c31f271bcdc8925f30f0
PutriHabibahDevina <putrihdm13@gmail.com>
2025-01-27 14:05:02 +0700

    Menambahkan seralizable untuk data class pengembalian

--- app/src/main/java/com/example/uas/model/Pengembalian.kt
+++ app/src/main/java/com/example/uas/model/Pengembalian.kt
@@ -1,5 +1,8 @@
 package com.example.uas.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class Pengembalian (
     val id_pengembalian:String,
     val id_peminjaman:String,
