b26e4c24de32af161ea0d04bdbc433f00d01444c
PutriHabibahDevina <putrihdm13@gmail.com>
2025-01-28 23:20:54 +0700

    Menambahkan data di gradle dan xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-compose:2.7.5")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.0")
 }
