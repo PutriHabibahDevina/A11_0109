75b59f37bbe99f1d2538320d4eafd972955cc515
PutriHabibahDevina <putrihdm13@gmail.com>
2025-01-28 10:12:32 +0700

    Menambahkan fungsi update dan delete di repostori pengembalian

--- app/src/main/java/com/example/uas/repository/PengembalianRepository.kt
+++ app/src/main/java/com/example/uas/repository/PengembalianRepository.kt
@@ -7,6 +7,8 @@ import java.io.IOException
 interface PengembalianRepository {
     suspend fun getPengembalian(): List<Pengembalian>
     suspend fun insertPengembalian(pengembalian: Pengembalian)
+    suspend fun updatePengembalian(id_pengembalian: String, pengembalian: Pengembalian)
+    suspend fun deletePengembalian(id_pengembalian: String)
     suspend fun getPengembalianById(id_pengembalian: String): Pengembalian
 }
 
@@ -15,6 +17,24 @@ class NetworkPengembalianRepository(
 ) : PengembalianRepository {
     override suspend fun insertPengembalian(pengembalian: Pengembalian) {
         pengembalianApiService.insertPengembalian(pengembalian)
+    }
+
+    override suspend fun updatePengembalian(id_pengembalian: String, pengembalian: Pengembalian) {
+        pengembalianApiService.updatePengembalian(id_pengembalian, pengembalian)
+    }
+
+    override suspend fun deletePengembalian(id_pengembalian: String) {
+        try {
+            val response = pengembalianApiService.deletePengembalian(id_pengembalian)
+            if (!response.isSuccessful) {
+                throw IOException("Failed to delete Pengembalian. HTTP Status code: ${response.code()}")
+            } else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
     }
 
     override suspend fun getPengembalian(): List<Pengembalian> = pengembalianApiService.getPengembalian()
