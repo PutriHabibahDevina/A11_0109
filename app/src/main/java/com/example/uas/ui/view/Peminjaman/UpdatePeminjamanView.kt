8948fb58ed1d855fb5711dae23b10e2e6f760ee5
PutriHabibahDevina <putrihdm13@gmail.com>
2025-01-28 23:19:09 +0700

    Menambahkan fungsi entry di vody peminjaman

--- app/src/main/java/com/example/uas/ui/view/Peminjaman/UpdatePeminjamanView.kt
+++ app/src/main/java/com/example/uas/ui/view/Peminjaman/UpdatePeminjamanView.kt
@@ -1,15 +1,21 @@
 package com.example.uas.ui.view.Peminjaman
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uas.ui.customwidget.CustomeTopAppBar
 import com.example.uas.ui.navigation.DestinasiNavigasi
+import com.example.uas.ui.viewmodel.Peminjaman.InsertUiPeminjaman
 import com.example.uas.ui.viewmodel.Peminjaman.UpdatePeminjamanViewModel
 import com.example.uas.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.launch
@@ -41,7 +47,7 @@ fun UpdatePeminjamanView(
         }
     ) { innerPadding ->
         EntryBodyPeminjaman(
-            insertPeminjamanUiState = viewModel.uiState,
+            insertPeminjamanUiState = viewModel.uiState.insertUiPeminjaman,
             onPeminjamanValueChange = viewModel::updateInsertPeminjamanState,
             onSaveClick = {
                 coroutineScope.launch {
@@ -53,5 +59,69 @@ fun UpdatePeminjamanView(
                 .padding(innerPadding)
                 .fillMaxWidth()
         )
+    }
+}
+
+@Composable
+fun EntryBodyPeminjaman(
+    insertPeminjamanUiState: InsertUiPeminjaman,
+    onPeminjamanValueChange: (InsertUiPeminjaman) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = insertPeminjamanUiState.id_peminjaman,
+            onValueChange = { newValue ->
+                onPeminjamanValueChange(insertPeminjamanUiState.copy(id_peminjaman = newValue))
+            },
+            label = { Text("ID Peminjaman") },
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = insertPeminjamanUiState.id_buku,
+            onValueChange = { newValue ->
+                onPeminjamanValueChange(insertPeminjamanUiState.copy(id_buku = newValue))
+            },
+            label = { Text("ID Buku") },
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+        )
+        OutlinedTextField(
+            value = insertPeminjamanUiState.id_anggota,
+            onValueChange = { newValue ->
+                onPeminjamanValueChange(insertPeminjamanUiState.copy(id_anggota = newValue))
+            },
+            label = { Text("ID Anggota") },
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+        )
+        OutlinedTextField(
+            value = insertPeminjamanUiState.tanggal_peminjaman,
+            onValueChange = { newValue ->
+                onPeminjamanValueChange(insertPeminjamanUiState.copy(tanggal_peminjaman = newValue))
+            },
+            label = { Text("ID Anggota") },
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+        )
+        OutlinedTextField(
+            value = insertPeminjamanUiState.tanggal_pengembalian,
+            onValueChange = { newValue ->
+                onPeminjamanValueChange(insertPeminjamanUiState.copy(tanggal_pengembalian = newValue))
+            },
+            label = { Text("ID Anggota") },
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+        )
+
+        Button(
+            onClick = onSaveClick,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        ) {
+            Text("Save")
+        }
     }
 }
